4e5be1a69cded4a0bd39e717acf39fe019c3c065
Ramon Liao <ramonliao.yc@gmail.com>
2024-01-30 18:12:03 +0000

    create image editor app

---
+++ ui5-template/webapp/Component.js
@@ -0,0 +1,14 @@
+sap.ui.define(["sap/ui/core/UIComponent"], (UIComponent) => {
+  "use strict";
+
+  return UIComponent.extend("ui5.imgeditor.Component", {
+    metadata: {
+      interfaces: ["sap.ui.core.IAsyncContentCreation"],
+      manifest: "json",
+    },
+    init() {
+      // call the init function of the parent
+      UIComponent.prototype.init.apply(this, arguments);
+    },
+  });
+});
